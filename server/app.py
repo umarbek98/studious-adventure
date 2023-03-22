a4feb26e63f00d8fdadc85e5c8a4b54992f0df4e
Ben Cavins <ben.cavins@flatironschool.com>
2023-03-22 11:00:00 -0400

    add imports

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 
 from models import db, Hero, HeroPower, Power
